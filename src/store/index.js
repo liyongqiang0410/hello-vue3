75a62bb40e71254963887260b19e2e8887ed1c1e
liyongqiang0410 <1825475869@qq.com>
2021-03-20 14:35:52 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,11 @@ import { createStore } from 'vuex'
 const modules = {}
 // 自动引入全部模块
 const requireModules = require.context('./modules', false, /.js$/)
+
 requireModules.keys().forEach(fileName => {
   modules[fileName.replace(/(\.\/|\.js)/g, '')] = requireModules(fileName).default
 });
+
 Object.keys(modules).forEach((key) => {
   modules[key]['namespaced'] = true
 })
